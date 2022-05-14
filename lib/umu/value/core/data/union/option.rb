e92a2daf9a9ac597cacf73210683eeea9663d79b
takomachan <88935217+takomachan@users.noreply.github.com>
2022-05-14 22:19:58 +0900

    Refactoring a module: VCDU::Option

--- lib/umu/value/core/data/union/option.rb
+++ lib/umu/value/core/data/union/option.rb
@@ -15,18 +15,23 @@ module Option
 
 class Abstract < Union::Abstract
 	CLASS_METHOD_INFOS = [
-		[:meth_make_some,	self,
-			:'make-some',	VC::Top],
 		[:meth_make_none,	self,
-			:'make-none']
+			:'make-none'],
+		[:meth_make_some,	self,
+			:'make-some',	VC::Top]
 	]
 
 	INSTANCE_METHOD_INFOS = [
-		[:meth_some?,		VCB::Bool,
-			:some?],
 		[:meth_none?,		VCB::Bool,
-			:none?]
+			:none?],
+		[:meth_some?,		VCB::Bool,
+			:some?]
 	]
+
+
+	def self.meth_make_none(env, _event)
+		VC.make_none L.make_position(__FILE__, __LINE__)
+	end
 
 
 	def self.meth_make_some(env, _event, contents)
@@ -36,18 +41,37 @@ class Abstract < Union::Abstract
 	end
 
 
-	def self.meth_make_none(env, _event)
-		VC.make_none L.make_position(__FILE__, __LINE__)
+	def meth_none?(env, event)
+		VC.make_false self.pos
 	end
 
 
 	def meth_some?(env, event)
-		raise X::SubclassResponsibility
+		VC.make_false self.pos
+	end
+end
+
+
+
+class None < Abstract
+	INSTANCE_METHOD_INFOS = [
+		[:meth_contents,	VC::Unit,
+			:contents]
+	]
+
+
+	def to_s
+		'NONE'
+	end
+
+
+	def meth_to_string(env, event)
+		VC.make_string self.pos, self.to_s
 	end
 
 
 	def meth_none?(env, event)
-		raise X::SubclassResponsibility
+		VC.make_true self.pos
 	end
 end
 
@@ -84,42 +108,8 @@ class Some < Abstract
 	end
 
 
-	def meth_none?(env, event)
-		VC.make_false self.pos
-	end
-
-
 	def meth_contents(_env, _event)
 		self.contents
-	end
-end
-
-
-
-class None < Abstract
-	INSTANCE_METHOD_INFOS = [
-		[:meth_contents,	VC::Unit,
-			:contents]
-	]
-
-
-	def to_s
-		'NONE'
-	end
-
-
-	def meth_to_string(env, event)
-		VC.make_string self.pos, self.to_s
-	end
-
-
-	def meth_some?(env, event)
-		VC.make_false self.pos
-	end
-
-
-	def meth_none?(env, event)
-		VC.make_true self.pos
 	end
 end
 
@@ -132,18 +122,18 @@ end	# Umu::Core::Data
 
 module_function
 
+	def make_none(pos)
+		ASSERT.kind_of pos,	L::Position
+
+		Data::Union::Option::None.new(pos).freeze
+	end
+
+
 	def make_some(pos, contents)
 		ASSERT.kind_of pos,			L::Position
 		ASSERT.kind_of contents,	VC::Top
 
 		Data::Union::Option::Some.new(pos, contents).freeze
-	end
-
-
-	def make_none(pos)
-		ASSERT.kind_of pos,	L::Position
-
-		Data::Union::Option::None.new(pos).freeze
 	end
 
 end	# Umu::Core
