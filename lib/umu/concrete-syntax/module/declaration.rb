e7a045bac5597dfa957dcbb6d831f7ffc13b401f
takomachan <88935217+takomachan@users.noreply.github.com>
2024-06-12 23:29:42 +0900

    Added a module: CSMD::Import::Field

--- lib/umu/concrete-syntax/module/declaration.rb
+++ lib/umu/concrete-syntax/module/declaration.rb
@@ -61,7 +61,131 @@ end
 
 
 
-class Import < Abstract
+module Import
+
+module Field
+
+class Abstract < Abstraction::Model
+	attr_reader :fields
+
+
+	def initialize(loc, fields)
+		ASSERT.kind_of	fields,	::Array
+
+		super(loc)
+
+		@fields = fields
+	end
+
+
+	def to_s
+		self.fields.map { |target_id, opt_source_id|
+			ASSERT.kind_of     target_id,		CSME::Identifier::Short
+			ASSERT.opt_kind_of opt_source_id,	CSME::Identifier::Abstract
+
+			format("%%%s %s%s",
+					__reserved_word__,
+
+					target_id.to_s,
+
+					if opt_source_id
+						format " = %s", opt_source_id
+					else
+						''
+					end
+			)
+		}.join(' ')
+	end
+
+
+	def desugar_field(env, souce_dir_id)
+		ASSERT.kind_of souce_dir_id, CSME::Identifier::Abstract
+
+		self.fields.map { |target_id, opt_source_id|
+			ASSERT.kind_of     target_id,     CSME::Identifier::Short
+			ASSERT.opt_kind_of opt_source_id, CSME::Identifier::Abstract
+
+			expr = ASCE.make_long_identifier(
+				souce_dir_id.loc,
+
+				souce_dir_id.head.desugar(env),
+
+				(
+					souce_dir_id.tail + (
+						opt_source_id ? opt_source_id : target_id
+					).to_a
+				).map { |id| id.desugar env }
+			)
+
+			ASCD.make_value(
+				target_id.loc,
+				target_id.sym,
+				expr,
+				__opt_type_sym__
+			)
+		}
+	end
+
+
+private
+
+	def __reserved_word__
+		raise X::SubclassResponsibility
+	end
+
+	def __opt_type_sym__
+		raise X::SubclassResponsibility
+	end
+end
+
+
+
+class Value < Abstract
+
+private
+
+	def __reserved_word__
+		'VAL'
+	end
+
+	def __opt_type_sym__
+		nil
+	end
+end
+
+
+
+class Function < Abstract
+
+private
+
+	def __reserved_word__
+		'FUN'
+	end
+
+	def __opt_type_sym__
+		:Function
+	end
+end
+
+
+
+class Structure < Abstract
+
+private
+
+	def __reserved_word__
+		'STRUCTURE'
+	end
+
+	def __opt_type_sym__
+		:Struct
+	end
+end
+
+end	# Umu::ConcreteSyntax::Module::Declaration::Import::Field
+
+class Entry < Declaration::Abstract
 	attr_reader :id, :opt_fields
 
 
@@ -78,29 +202,12 @@ class Import < Abstract
 
 	def to_s
 		body = if self.opt_fields
-			fields = self.opt_fields
+					fields = self.opt_fields
 
-			format(" { %s }",
-				fields.map { |target_id, opt_source_id|
-					ASSERT.kind_of     target_id,
-										CSME::Identifier::Short
-					ASSERT.opt_kind_of opt_source_id,
-										CSME::Identifier::Abstract
-
-					format("%%VAL %s%s",
-							target_id.to_s,
-
-							if opt_source_id
-								format " = %s", opt_source_id
-							else
-								''
-							end
-					)
-				}.join(' ')
-			)
-		else
-			''
-		end
+					format " { %s }", fields.map(&:to_s).join(' ')
+				else
+					''
+				end
 
 		format "%%IMPORT %s%s", self.id.to_s, body
 	end
@@ -119,23 +226,10 @@ private
 		result = if self.opt_fields
 			fields = self.opt_fields
 
-			decls = fields.map { |target_id, opt_source_id|
-				ASSERT.kind_of     target_id,     CSME::Identifier::Short
-				ASSERT.opt_kind_of opt_source_id, CSME::Identifier::Abstract
+			decls = fields.inject([]) { |array, field|
+				ASSERT.kind_of field, Field::Abstract
 
-				expr = ASCE.make_long_identifier(
-					self.loc,
-
-					self.id.head.desugar(new_env),
-
-					(
-						self.id.tail + (
-							opt_source_id ? opt_source_id : target_id
-						).to_a
-					).map { |id| id.desugar(new_env) }
-				)
-
-				ASCD.make_value self.loc, target_id.sym, expr
+				array + field.desugar_field(new_env, self.id)
 			}
 
 			ASCD.make_declarations self.loc, decls
@@ -146,6 +240,8 @@ private
 		ASSERT.kind_of result, ASCD::Abstract
 	end
 end
+
+end	# Umu::ConcreteSyntax::Module::Declaration::Import
 
 
 
@@ -195,7 +291,28 @@ module_function
 		ASSERT.kind_of		id,			CSME::Identifier::Abstract
 		ASSERT.opt_kind_of	opt_fields,	::Array
 
-		Import.new(loc, id, opt_fields.freeze).freeze
+		Import::Entry.new(loc, id, opt_fields.freeze).freeze
+	end
+
+
+	def make_value_field_of_import(loc, atomic_fields)
+		ASSERT.kind_of atomic_fields, ::Array
+
+		Import::Field::Value.new(loc, atomic_fields.freeze).freeze
+	end
+
+
+	def make_function_field_of_import(loc, atomic_fields)
+		ASSERT.kind_of atomic_fields, ::Array
+
+		Import::Field::Function.new(loc, atomic_fields.freeze).freeze
+	end
+
+
+	def make_structure_field_of_import(loc, atomic_fields)
+		ASSERT.kind_of atomic_fields, ::Array
+
+		Import::Field::Structure.new(loc, atomic_fields.freeze).freeze
 	end
 
 
