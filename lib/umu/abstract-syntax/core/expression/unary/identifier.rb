aa0bd2ffdeab8f79eeaf307d66400f1914e56321
takomachan <88935217+takomachan@users.noreply.github.com>
2024-06-24 01:22:19 +0900

    Bugfix for a class: ASCEU::Identifier::Long

--- lib/umu/abstract-syntax/core/expression/unary/identifier.rb
+++ lib/umu/abstract-syntax/core/expression/unary/identifier.rb
@@ -82,13 +82,41 @@ private
         ASSERT.kind_of env, E::Entry
 
         init_value = env.va_lookup self.head_id.sym, self.head_id.loc
-        ASSERT.kind_of init_value, VC::Struct::Entry
+        ASSERT.kind_of init_value, VC::Top
 
-        final_value = self.tail_ids.inject(init_value) { |value, id|
-            ASSERT.kind_of value,   VC::Struct::Entry
-            ASSERT.kind_of id,      Short
+        unless init_value.kind_of? VC::Struct::Entry
+            raise X::TypeError.new(
+                self.head_id.loc,
+                env,
+                "For identifier: '%s', expected a Struct, but %s : %s",
+                    self.head_id.sym.to_s,
+                    init_value.to_s,
+                    init_value.type_sym.to_s
+            )
+        end
 
-            value.select id.sym, id.loc, env
+        final_value, _sym = self.tail_ids.inject(
+             [init_value, self.head_id.sym]
+        ) {
+            |(value,      parent_sym      ), id|
+            ASSERT.kind_of value,      VC::Top
+            ASSERT.kind_of parent_sym, ::Symbol
+            ASSERT.kind_of id,         Short
+
+            #pp({value: value.class, sym: parent_sym, id: id})
+
+            unless value.kind_of? VC::Struct::Entry
+                raise X::TypeError.new(
+                    id.loc,
+                    env,
+                    "For identifier: '%s', expected a Struct, but %s : %s",
+                        parent_sym.to_s,
+                        value.to_s,
+                        value.type_sym.to_s
+                )
+            end
+
+            [value.select(id.sym, id.loc, env), id.sym]
         }
         ASSERT.kind_of final_value, VC::Top
     end
