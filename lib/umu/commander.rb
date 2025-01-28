40b6e117c82cd415f3fcce2d896e83ea57bcd226
takomachan <88935217+takomachan@users.noreply.github.com>
2025-01-28 17:53:45 +0900

    Renamed a file: 'commander/commander.rb' -> 'entry.rb'

--- lib/umu/commander.rb
+++ lib/umu/commander.rb
@@ -4,4 +4,4 @@
 require_relative 'commander/option'
 require_relative 'commander/subcommand'
 require_relative 'commander/prelude'
-require_relative 'commander/commander'
+require_relative 'commander/entry'
