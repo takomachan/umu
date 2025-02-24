efcf1651fc77740289ae76e206324fa7c9fc7803
takomachan <88935217+takomachan@users.noreply.github.com>
2025-02-24 23:01:44 +0900

    Add Users Guide and Programming Guide to 'doc/'

--- doc/tmdoc/make-Make.FILES.sh
+++ doc/tmdoc/make-Make.FILES.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-rake -C ../lib/umu -f Rakefile files | \
+rake -C ../../lib/umu -f Rakefile files | \
     awk 'BEGIN { printf "FILES = \\\n" }
          { printf "\t%s \\\n", $1 }
     ' > Make.FILES
