7085906b11596e3a97de5f1ca49df55ee5835485
takomachan <88935217+takomachan@users.noreply.github.com>
2025-03-19 01:17:45 +0900

    Update 'test/lexcical_test.rb'

--- lib/umu/lexical/lexer.rb
+++ lib/umu/lexical/lexer.rb
@@ -16,13 +16,13 @@ require_relative 'lexer/entry'
 =begin
 The lexcical analyzer is modeled as a finite state machine with a stack.
 Here we show the definition of a model using a fictitious domain
-escription language.
-This domain description language was inspired by Estelle,
+specific language (DSL).
+This domain specific language was inspired by Estelle,
 a formal specification description language for communication protocols.
 
 
 字句解析器は、スタック付き有限状態機械としてモデル化される。
-ここでは、架空のドメイン記述言語によるモデルの定義を示す。
+ここでは、架空のドメイン固有言語(DSL)によるモデルの定義を示す。
 このドメイン記述言語は、通信プロトコルの
 形式的仕様記述言語であるEstelleから発想を得て考案した。
 
@@ -38,150 +38,164 @@ Estelleに関する文献を以下に示す。
 ##### State Machine Specification  ####
 #######################################
 
-    %DATA Token =
-      TkNewline     (String, Int)
-    | TkSpace       (String, Int)
-    | TkComment     (String, Int)
-    | TkBeginBraket (String, Int)
-    | TkEndBraket   (String, Int)
-    | TkNumber      (Number, Int)
-    | TkReserved    (Symbol, Int)
-    | TkIdentifier  (Symbol, Int)
-    | TkSymbol      (Symbol, Int)
-    | TkString      (String, Int)
+%STRUCTURE Lexer {
+    %DATA Pattern {
+      | PatBeginComment  String
+      | PatEndComment    String
+      | PatNewline       String
+      | PatSpace         String
+      | PatBeginString   String
+      | PatEndString     String
+      | PatBeginBraket   String
+      | PatEndBraket     String
+      | PatNumber        String
+      | PatWord          String
+      | Any              String
+    }
 
-    %DATA Pattern =
-      BeginComment  String
-    | EndComment    String
-    | Newline       String
-    | Space         String
-    | BeginString   String
-    | EndString     String
-    | BeginBraket   String
-    | EndBraket     String
-    | Number        String
-    | Word          String
-    | Any           String
+    %DATA Token {
+      | TokNewline     (String, Int)    /* NL(String)       */
+      | TokSpace       (String, Int)    /* SP(String)       */
+      | TokComment     (String, Int)    /* COMMENT(String)  */
+
+      | TokBeginBraket (String, Int)
+      | TokEndBraket   (String, Int)
+
+      | TokReserved    (Symbol, Int)    /* IF, THEN, ELSE, ... etc */
+
+      | TokIdentifier  (Symbol, Int)    /* ID(Symbol) or    */
+                                        /* MSG(Symbol) or   */
+                                        /* LABEL(Symbol) or */
+                                        /* DIR(Symbol) or   */
+                                        /* NSEL(Int) or     */
+                                        /* LSEL(Symbol) or  */
+
+      | TokNumber      (Number, Int)    /* INT(Int) or      */
+                                        /* FLOAT(Float)     */
+
+      | TokSymbol      (Symbol, Int)    /* SYM(Symbol)      */
+      | TokString      (String, Int)    /* STRING(String)   */
+    }
 
     %INPUT %EVENT  = Pattern
     %OUTPUT %EVENT = Token
 
 
-    %ABSTRACT %STATE Abstract {
-        line-num:       Int,
-        braket-stack:   String List
-    }
-    %ABSTRACT %STATE AbstractString %IS-A Abstract {
-        buf:            String
+    %STRUCTURE Abstraction {
+        %ABSTRACT %STATE Abstract %HAS {
+            line-num:       Int,
+            braket-stack:   String List
+        }
+
+        %ABSTRACT %STATE String %IS-A Abstract %HAS {
+            buf:            String
+        }
     }
 
-    %STATE Separator %IS-A Abstract
-    %STATE Comment   %IS-A Abstract {
+    %STATE Separator %IS-A Abstraction::Abstract
+
+    %STATE Comment %IS-A Abstraction::Abstract %HAS {
         buf:            String,
         saved-line-num: Int,
         comment-depth:  Int
     }
-    %STATE Token     %IS-A Abstract
 
-    %STATE BasicString      %IS-A AbstractString
-    %STATE SymbolizedString %IS-A AbstractString
+    %STATE Token %IS-A Abstraction::Abstract
 
-    %INITIAL %STATE Separator {line-num: 1, braket-stack: []}
+    %STATE BasicString      %IS-A Abstraction::String
+    %STATE SymbolizedString %IS-A Abstraction::String
+
+    %INITIAL %STATE = %NEW Separator {line-num: 1 braket-stack: []}
 
     %VAL braket-map = %{"(" -> ")", "[" -> "]", "{" -> "}", ....}
 
 
     %TRANSITION {
       %FROM Separator {line-num:}
-        %WHEN BeginComment _
+        %WHEN PatBeginComment _
             %TO Comment {
-                    buf: "", saved-line-num: line-num, comment-depth: 1
+                    buf: "" saved-line-num: line-num comment-depth: 1
                 }
-        %WHEN Newline matched
-            %OUTPUT TkNewline (matched, line-num)
+        %WHEN PatNewline matched
+            %OUTPUT TokNewline (matched, line-num)
             %TO %SAME {line-num: line-num + 1}
-        %WHEN Space matched
-            %OUTPUT TkSpace (matched, line-num)
+        %WHEN PatSpace matched
+            %OUTPUT TokSpace (matched, line-num)
         %WHEN %ANY
             %TO Token
     | %FROM Comment {
-            buf:,
-            saved-line-num: saved-line-num,
+            buf:
+            saved-line-num: saved-line-num
             comment-depth: depth
-            ..
         }
-        %WHEN BeginComment matched
-            %TO %SAME {buf: buf ^ matched, comment-depth: depth + 1}
-        %WHEN EndComment matched
-            %IF depth <= 1 %THEN
-                %OUTPUT TkComment (buf, saved-line-num)
+        %WHEN PatBeginComment matched
+            %TO %SAME {buf: buf ^ matched comment-depth: depth + 1}
+        %WHEN PatEndComment matched
+            ! %IF depth <= 1 %THEN
+                %OUTPUT TokComment (buf, saved-line-num)
                 %TO Separator
-            %ELSE
-                %TO %SAME {buf: buf ^ matched, comment-depth: depth - 1}
-            %END
-        %WHEN Newline matched
-            %TO %SAME {line-num: line-num + 1, buf: buf ^ matched}
-        %WHEN Any matched
+              %ELSE
+                %TO %SAME {buf: buf ^ matched comment-depth: depth - 1}
+        %WHEN PatNewline matched
+            %TO %SAME {line-num: line-num + 1 buf: buf ^ matched}
+        %WHEN PatAny matched
             %TO %SAME {buf: buf ^ matched}
         %WHEN %ANY
-            %ABORT
-    | %FROM Token {line-num:, braket-stack: stack}
-        %WHEN BeginString
-            %IF symbol?
+            %ABORT "No case"
+    | %FROM Token {line-num: braket-stack: stack}
+        %WHEN PatBeginString
+            ! %IF symbol?
                 %TO SymbolizedString {buf: ""}
-            %ELSE
+              %ELSE
                 %TO BasicString {buf: ""}
-            %END
-        %WHEN BeginBraket matched
+        %WHEN PatBeginBraket matched
             %OUTPUT BeginBraket (matched, line-num)
             %TO Separator {braket-stack: [matched|stack]}
-        %WHEN EndBraket matched-eb
-            %CASE stack %OF
-               [] -> %ERROR "Unexpected end-braket"
-            %OR [bb|stack'] ->
-                %CASE braket-map.(lookup bb) %OF
-                   NONE -> %ABORT
-                %OR Some found-eb ->
-                    %IF matched-eb == found-eb %THEN
-                        %OUTPUT EndBraket (matched-eb, line-num)
-                        %TO Separator {braket-stack: stack'}
-                    %ELSE
-                        %ERROR "Mismatched brakets"
-                    %END
-                %END
-            %END
-        %WHEN Number matched
-            %OUTPUT TkNumber (to-number matched, line-num)
+        %WHEN PatEndBraket matched-eb
+            ! %CASE stack %OF {
+                [] -> %ERROR "Unexpected end-braket"
+              | [bb|stack'] ->
+                    %CASE braket-map.(lookup bb) %OF {
+                      | NONE -> %ABORT ("Unknown bracket: " ^ bb)
+                      | Some found-eb ->
+                            %IF matched-eb == found-eb %THEN
+                                %OUTPUT EndBraket (matched-eb, line-num)
+                                %TO Separator {braket-stack: stack'}
+                            %ELSE
+                                %ERROR "Mismatched brakets"
+                    }
+            }
+        %WHEN PatNumber matched
+            %OUTPUT TokNumber (to-number matched, line-num)
             %TO Separator
-        %WHEN Word matched
-            %IF symbol?
-                %OUTPUT TkSymbol (to-symbol matched, line-num)
-            %ELSE
-                %IF reserved?
-                    %OUTPUT TkReserved (to-symbol matched, line-num)
+        %WHEN PatWord matched
+            ! %IF symbol? %THEN
+                %OUTPUT TokSymbol (to-symbol matched, line-num)
+              %ELSE
+                %IF reserved? %THEN
+                    %OUTPUT TokReserved (to-symbol matched, line-num)
                 %ELSE
-                    %OUTPUT TkIdentifier (to-symbol matched, line-num)
-                %END
-            %END
+                    %OUTPUT TokIdentifier (to-symbol matched, line-num)
             %TO Separator
         %WHEN %ANY
             %ERROR "Can't recognized as token"
     | %FROM AbstractString {buf: ..}
-        %WHEN Newline
+        %WHEN PatNewline
             %ERROR "Unexpected end-of-string"
-        %WHEN Any matched
+        %WHEN PatAny matched
             %TO %SAME {buf: buf ^ matched}
     | %FROM BasicString {line-num:, buf: ..}
-        %WHEN EndString
-            %OUTPUT TkString (buf, line-num)
+        %WHEN PatEndString
+            %OUTPUT TokString (buf, line-num)
             %TO Separator
         %WHEN %ANY
             %ABORT
     | %FROM SymbolizedString {line-num:, buf: ..}
-        %WHEN EndString
-            %OUTPUT TkSymbol (buf, line-num)
+        %WHEN PatEndString
+            %OUTPUT TokSymbol (buf, line-num)
             %TO Separator
         %WHEN %ANY
             %ABORT
     }
+}
 =end
--- test/lexcical_test.rb
+++ test/lexcical_test.rb
@@ -8,6 +8,110 @@ module Umu
 module Test
 
 module Lexcical
+=begin
+/* See state machine specification in "lib/umu/lexical/lexer.rb" */
+
+
+/* Token (Entry rule of Lexcical ruleset) */
+
+<token> ::=
+    <separator>
+  | <number>
+  | <string>
+  | <word>
+  ;
+
+
+/* Separator */
+
+<separator> ::=
+    <space>
+  | <newlinw>
+  | <comment>
+  ;
+
+<space>   ::= " "+                 ;    /* PatSpace */
+<newline> ::= "\n" | "\r" | "\n\r" ;    /* PatNewline */
+<comment> ::=
+    <line-comment>
+  | <begein-comment>
+  | <end-comment>
+  ;
+
+<line-comment>  ::= "#" ( ^ <newlinw> )* ;   /* PatComment       */
+<begin-comment> ::= "(#"                 ;   /* PatBeginComment  */
+<end-comment>   ::= "#)"                 ;   /* PatEndComment    */
+
+
+/* Number */
+
+<number> ::= <int> | <float> ;
+
+<int>   ::= <sign>? <digit>+              ;  /* PatNumber */
+<float> ::= <sign>? <digit>+ "." <digit>+ ;  /* PatNumber */
+
+<sign> ::= "+" | "-" ;
+
+
+/* String */
+
+<string> ::=
+    <begin-string>
+  | <begin-symbolized-string>
+  | <end-string> ;
+
+<begin-string>           ::= "\""     ;  /* PatBeginString  */
+<begin-symbolizedstring> ::= "@" "\"" ;  /* PatBeginString  */
+<end-string>             ::= "\""     ;  /* PatEndString    */
+
+
+/* Word */
+
+<word> ::=
+/*  <reserved>  -- Reserved words are not defined in test suite */
+  | <id>
+  | <symbol>
+  | <message>
+  | <label>
+  | <directory>
+  | <symbol>
+  | <selector>
+  ;
+
+<id>        ::= <id-word>      ;    /* PatWord */
+<symbol>    ::= "@" <id-word>  ;    /* PatWord */
+<message>   ::= "." <id-word>  ;    /* PatWord */
+<label>     ::= <id-word> ":"  ;    /* PatWord */
+<directory> ::= <id-word> "::" ;    /* PatWord */
+
+<basic-symbol>      ::= "@" <id-word> ; /* PatWord */
+<symbolized-symbol> ::= "@" <string>  ; /* PatWord */
+
+<selector> ::=
+    <number-selector>
+  | <label-selector>
+  ;
+
+<number-selector> ::= "$" <digit>+  ;    /* PatWord */
+<label-selector>  ::= "$" <id-word> ;    /* PatWord */
+
+
+/* Common rules */
+
+<id-ward> ::=
+    "_"*
+    <alpha> <alnum>*
+    ( "-" <alnum>+ )*
+    "_"*
+    ( "?" | "!" )
+    "'"*
+    "_"*
+    ;
+
+<alnum> ::= <alpha> | <digit>   ;
+<alpha> ::= "a" .. "z" | "A" .. "Z" ;
+<digit> ::= "0" .. "9"              ;
+=end
 
 class LexcicalTest < Minitest::Test
     def setup
@@ -63,13 +167,6 @@ class LexcicalTest < Minitest::Test
         value = Api.eval_expr @interp, '@"Hello world"'
         assert_instance_of  VCA::Symbol,    value
         assert_equal        :"Hello world", value.val
-    end
-
-
-    def test_variable
-        value = Api.eval_expr @interp, "TRUE"
-        assert_instance_of  VCA::Bool, value
-        assert_equal        true,      value.val
     end
 
 
